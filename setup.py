700ea5802c34f7d6b51f7c737d4dd0eb9836a747
lmenou <menl94629@gmail.com>
2022-07-26 18:35:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
-__version__ = '1.0'
-__author__ = 'Louis Manchon'
+__version__ = "0.0.1"
+__author__ = "Lucas Menou and Louis Manchon"
 
-setup(name = 'figure_size',
-      version = __version__,
-      description = 'Figure Size Utilities for LaTeX',
-      author = __author__,
-      packages = ['LaTeXfigsizer'])
+setup(
+    name="laTeXfigsizer",
+    version=__version__,
+    description="Figure Size Utilities for LaTeX",
+    author=__author__,
+    packages=["LaTeXfigsizer"],
+)
