f2a7e67ccd6ea27f3dcfc69cc75a9694bf221169
lmenou <menl94629@gmail.com>
2022-07-27 11:52:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
-from distutils.core import setup
+"""A setuptools based for latexfigsizer.
+"""
 
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
 __version__ = "0.0.1"
 __author__ = "narsonalin and lmenou"
 
@@ -7,6 +14,18 @@ setup(
     name="latexfigsizer",
     version=__version__,
     description="Figure Size Utilities for LaTeX",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
     author=__author__,
-    packages=["latexfigsizer"],
+    # author_email="author@example.com",
+    keywords="latex, figsize, subplots",
+    package_dir={"": "latexfigsizer"},
+    packages=find_packages(where="latexfigsizer"),
+    python_requires=">=3.8, <4",
+    # install_requires=["peppercorn"],
+    # extras_require={
+    #     "dev": ["check-manifest"],
+    #     "test": ["coverage"],
+    # },
 )
