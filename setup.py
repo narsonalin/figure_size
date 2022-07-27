72eace0cc9814291bdefa0e6b2002f6efaf7f65e
lmenou <menl94629@gmail.com>
2022-07-27 09:32:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-#!/anaconda/bin/python3
-
 from distutils.core import setup
 
-__version__ = '1.0'
-__author__ = 'Louis Manchon'
+__version__ = "0.0.1"
+__author__ = "narsonalin and lmenou"
 
-setup(name = 'figure_size',
-      version = __version__,
-      description = 'Figure Size Utilities',
-      author = __author__,
-      packages = ['figure_size'])
+setup(
+    name="latexfigsizer",
+    version=__version__,
+    description="Figure Size Utilities for LaTeX",
+    author=__author__,
+    packages=["latexfigsizer"],
+)
